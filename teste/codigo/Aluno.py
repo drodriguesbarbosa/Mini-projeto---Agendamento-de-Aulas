a7cc46b34967986e4b2d8d9b19e9752046a8fb5e
drodriguesbarbosa <drodriguesbarbosa@hotmail.com>
2022-05-13 19:06:02 -0300

    Correção nomes das variantes

--- teste/codigo/Aluno.py
+++ teste/codigo/Aluno.py
@@ -6,10 +6,5 @@ class Aluno(Pessoa):
         super().__init__(nome, idade)
         self.grau_escolar = grau_escolar
 
-a1 = Aluno('Daniela', 28, 'superior')
-a2 = Aluno('Julia', 29, 'Pós Graduação')
-a3 = Aluno('Gabriel', 16, 'Ensino Médio')
-
-print(a1.grau_escolar)
 
 
---
+++ teste/codigo/Aula.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+from Pessoa import Pessoa
+
+class Aula ():
+
+    def __init__(self, aluno, professor, horario):
+        self.aluno = aluno
+        self.professor = professor
+        self.horario = horario
+
+
+
+
